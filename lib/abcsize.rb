83daaa009f02025a91ef590260aba258bbc0635c
Emil Furtak <emil.furtak@gmail.com>
2021-11-28 16:47:31 +0100

    adds errors handling and changes variables naming

--- lib/abcsize.rb
+++ lib/abcsize.rb
@@ -9,15 +9,29 @@ module Abcsize
 
   # class responsible for returning ABC size from ABC size calculator
   class Abc
-    def self.size(path)
-      source = File.open(path, 'r').read
+    def size(path)
+      source = get_source_from_file(path)
       ruby_version = RuboCop::TargetRuby.supported_versions.last
 
       node = RuboCop::AST::ProcessedSource.new(source, ruby_version).ast
-      score = RuboCop::Cop::Metrics::Utils::AbcSizeCalculator.calculate(node)
 
-      puts  "ABC size: #{Rainbow(score[0]).red}, while: <= 17 satisfactory, 18..30 unsatisfactory, > 30 dangerous\n"\
-            "Assignments, branches (method calls), and conditions: #{Rainbow(score[1]).red}"
+      abc_size, abc = RuboCop::Cop::Metrics::Utils::AbcSizeCalculator.calculate(node)
+
+      puts  "ABC size: #{abc_size}, while: <= 17 satisfactory, 18..30 unsatisfactory, > 30 dangerous\n"\
+            "Assignments, branches (method calls), conditions: #{abc}"
+    end
+
+    private
+
+    def get_source_from_file(path)
+      file = File.open(path, 'r').read
+
+      raise Error, 'File is empty!' if file.empty?
+
+      file
+    rescue TypeError, Errno::ENOENT, Errno::EISDIR, Error => e
+      puts "#{e.message}\nPlease provide valid path to valid file."
+      exit
     end
   end
 end
