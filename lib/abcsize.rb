3b8e42d8ddca069e4b1c5d497ef3d0954798c585
Emil Furtak <emil.furtak@gmail.com>
2021-11-29 21:01:27 +0100

    re-formats result message, and colors it red when abc_size too high

--- lib/abcsize.rb
+++ lib/abcsize.rb
@@ -10,6 +10,8 @@ module Abcsize
 
   # class responsible for returning ABC size from ABC size calculator
   class Abc
+    SATISFACTORY_ABC_SIZE = 17
+
     def size(path)
       source = get_source_from_file(path)
       ruby_version = RuboCop::TargetRuby.supported_versions.last
@@ -17,16 +19,17 @@ module Abcsize
       nodes = RuboCop::AST::ProcessedSource.new(source, ruby_version).ast
 
       nodes.each_node { |n| return_result(n) if n.is_a?(RuboCop::AST::DefNode) }
+
+      print_interpretation
     end
 
     private
 
     def get_source_from_file(path)
-      file = File.open(path, 'r').read
+      content = File.open(path, 'r').read
+      raise Error, 'File is empty!' if content.empty?
 
-      raise Error, 'File is empty!' if file.empty?
-
-      file
+      content
     rescue TypeError, Errno::ENOENT, Errno::EISDIR, Error => e
       puts  "#{e.message}\n"\
             'Please provide valid path to valid file.'
@@ -36,13 +39,23 @@ module Abcsize
     def return_result(node)
       abc_size, abc = RuboCop::Cop::Metrics::Utils::AbcSizeCalculator.calculate(node)
 
-      puts  "Method name: #{r(node.method_name)}\n"\
-            "ABC size: #{r(abc_size)}, while: <= 17 satisfactory, 18..30 unsatisfactory, > 30 dangerous\n"\
-            "Assignments, branches (method calls), conditions: #{r(abc)}\n\n"
+      satisfactory = abc_size <= SATISFACTORY_ABC_SIZE
+
+      puts "ABC size: #{color(format('%.2f', abc_size), satisfactory)}, "\
+           "ABC: #{color(abc, satisfactory)} "\
+           "for method: #{color(node.method_name, satisfactory)}"
+
+      [abc_size, abc]
     end
 
-    def r(input)
-      Rainbow(input).yellow
+    def color(input, satisfactory)
+      satisfactory ? Rainbow(input).yellow : Rainbow(input).red
+    end
+
+    def print_interpretation
+      puts  "\n"\
+            "ABC size: <= 17 satisfactory, 18..30 unsatisfactory, > 30 dangerous\n"\
+            'ABC: <assignments, branches (method calls), conditions>'
     end
   end
 end
