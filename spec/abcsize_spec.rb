8c2dc951bf6ea1d1eca20b197f9784320c10c67a
Emil Furtak <emil.furtak@gmail.com>
2021-11-29 21:41:04 +0100

    removes exemplary example from spec

--- spec/abcsize_spec.rb
+++ spec/abcsize_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Abcsize do
   it 'has a version number' do
     expect(Abcsize::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
